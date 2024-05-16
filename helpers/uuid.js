3e4837185c4625eaee10fcb77088db95ca93b9e4
Saydeezr <saydeezr@gmail.com>
2024-05-16 15:11:07 -0400

    added middleware and fixed file path

--- helpers/uuid.js
+++ helpers/uuid.js
@@ -1,3 +1,4 @@
+//creates random id (need for each task)
 module.exports = () =>
     Math.floor((1 + Math.random()) * 0x10000)
       .toString(16)
--- server.js
+++ server.js
@@ -1,16 +1,34 @@
 const express = require('express');
 const path = require('path');
-const app = express();
+const uuid = require('./helpers/uuid');
 const PORT = process.env.PORT || 3000;
+const app = express();
 
-app.use(express.static('/public'));
+//making 'public' folder accesible to client 
+app.use(express.static('public'));
+app.use(express.json());
 
-app.get('/notes', (req,res) => res.sendFile(path.join(__dirname,'notes.html')));
-
-app.get('*', (req,res) => res.sendFile('index.html'));
+//brings user to next page
+app.get('/notes', (req,res) => res.sendFile(path.join(__dirname,'./public/notes.html')));
 
 app.get('/api/notes', (req,res) => res.json('db.json'));
 
-app.post('/api/notes', (req,res) => res.json())
+app.post('/api/notes', (req,res) => {
+    let noteData = require('./db/db.json')
+    //save the data to a vaiable
+    let newNote = (req.body)
+    //add unique id
+    
+    //.push() new note to noteData variable 
+    noteData.push(newNote);
+    //write new array into db.json
 
-app.listen(PORT)
+    //respond with new note
+
+})
+
+app.get('*', (req,res) => res.sendFile(path.join(__dirname,'./public/index.html')));
+
+app.listen(PORT, () => {
+    console.log(`server turned on`)
+})
